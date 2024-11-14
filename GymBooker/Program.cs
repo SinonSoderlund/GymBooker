ff3ac7b916fd975a4a3f541e6c11e8cdb75ef5a6
SinonSoderlund <sinonlynx@gmail.com>
2024-11-14 16:53:07 +0100

    v2.0.1

--- GymBooker/Program.cs
+++ GymBooker/Program.cs
@@ -33,7 +33,7 @@ namespace GymBooker
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/GymClasses/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -47,7 +47,7 @@ namespace GymBooker
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=GymClasses}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
